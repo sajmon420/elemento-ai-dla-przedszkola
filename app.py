6fa3c7de5d68b440c88484f48f39350bff3c43ba
sajmon420 <sajmoneqq69@gmail.com>
2026-02-22 14:42:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+pip install -q -U google-genai
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+)
+print(response.text)
 import google.generativeai as genai
 from google.api_core.exceptions import NotFound, InvalidArgument
 
